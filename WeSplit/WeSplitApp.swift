0f1f7a76f6943b627a7062eee9457a99d202973c
Paige Stephenson <paigestephenson29@gmail.com>
2023-05-24 19:05:52 -0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Paige Stephenson on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
